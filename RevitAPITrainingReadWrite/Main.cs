30a25e6db8867a5b08d3b4e78fd1759d49d30886
vladalexgor@ya.ru <vladalexgor@ya.ru>
2022-03-27 18:46:59 +0300

    Отработано занятие 4.7

--- RevitAPITrainingReadWrite/Main.cs
+++ RevitAPITrainingReadWrite/Main.cs
@@ -3,6 +3,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.DB.Mechanical;
 using Autodesk.Revit.UI;
+using Newtonsoft.Json;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using System;
@@ -24,64 +25,24 @@ namespace RevitAPITrainingReadWrite
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
-            OpenFileDialog openFileDialog1 = new OpenFileDialog
-            {
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                Filter = "Excel files(*.xlsx) | *.xlsx"
-            };
-
-            string filePath = string.Empty;
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                filePath = openFileDialog1.FileName;
-            }
-
-            if (string.IsNullOrEmpty(filePath))
-                return Result.Cancelled;
-
             var rooms = new FilteredElementCollector(doc)
                 .OfCategory(BuiltInCategory.OST_Rooms)
                 .Cast<Room>()
                 .ToList();
 
-            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            var roomDataList = new List<RoomData>();
+            foreach (var room in rooms)
             {
-                IWorkbook workbook = new XSSFWorkbook(filePath);
-                ISheet sheet = workbook.GetSheetAt(index: 0);
-
-                int rowIndex = 0;
-                while (sheet.GetRow(rowIndex) != null)
+                roomDataList.Add(new RoomData
                 {
-                    if (sheet.GetRow(rowIndex).GetCell(0) == null ||
-                        sheet.GetRow(rowIndex).GetCell(1) == null)
-                    {
-                        rowIndex++;
-                        continue;
-                    }
-
-
-                    string name = sheet.GetRow(rowIndex).GetCell(0).StringCellValue;
-                    string number = sheet.GetRow(rowIndex).GetCell(1).StringCellValue;
-
-                    var room = rooms.FirstOrDefault(r => r.Number.Equals(number));
-
-                    if (room == null)
-                    {
-                        rowIndex++;
-                        continue;
-                    }
-
-
-                    using (var ts = new Transaction(doc, "Set parameter"))
-                    {
-                        ts.Start();
-                        room.get_Parameter(BuiltInParameter.ROOM_NAME).Set(name);
-                        ts.Commit();
-                    }
-
-                    rowIndex++;
-                }
+                    Name = room.Name,
+                    Number = room.Number
+                });
             }
+
+            string json = JsonConvert.SerializeObject(roomDataList, Formatting.Indented);
+
+            File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "data.json"), json);
 
             return Result.Succeeded;
         }
